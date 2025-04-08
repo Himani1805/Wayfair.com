c3cfca959e46fc01b86b18cadc214b7449211177
Himani Sharma <hinusharma18@gmail.com>
2025-04-08 17:22:24 +0530

    Account menu is fix.

--- Frontend/src/components/Header/AccountDropdown.jsx
+++ Frontend/src/components/Header/AccountDropdown.jsx
@@ -1,9 +1,10 @@
 import { Box, Link } from "@chakra-ui/react";
 import { useState, useRef } from "react";
 import AccountMenu from "../Header/AccountMenu";
+import { MdOutlineAccountCircle } from "react-icons/md";
 
 const AccountDropdown = () => {
-    const [isOpen, setIsOpen] = useState(true);
+    const [isOpen, setIsOpen] = useState(false);
     const timeoutRef = useRef(null);
   
   
@@ -22,20 +23,22 @@ const AccountDropdown = () => {
     return (
       <Box position="relative">
         <Box
+          width={"100%"}
           display="flex"
           alignItems="center"
-          gap="5px"
+          // gap="5px"
           cursor="pointer"
-          p="8px"
+          // p="8px"
           borderRadius="md"
+          // border={"2px solid green"}
           _hover={{ bg: "gray.100" }}
           onMouseEnter={handleMouseEnter}
           onMouseLeave={handleMouseLeave}
         >
-          
-          <Link href="#" fontWeight="500" _hover={{ textDecoration: "none", color: "#7c189f" }}>
-            Account
-          </Link>
+           <MdOutlineAccountCircle _hover={{ color: "#7c189f" }} size={30} />
+          {/* <Link href="#" fontWeight="500" _hover={{ textDecoration: "none", color: "#7c189f" }}>
+           
+          </Link> */}
         </Box>
   
         {isOpen && (
@@ -43,13 +46,14 @@ const AccountDropdown = () => {
             position="absolute"
             top="40px"
             left="0"
+            marginLeft={"-100px"}
             bg="white"
             // boxShadow="md"
             p={4}
             borderRadius="md"
             minW="200px"
             zIndex="10"
-            // border="1px solid #ddd"
+            // border="1px solid red"
             onMouseEnter={handleMouseEnter} 
             onMouseLeave={handleMouseLeave} 
           >
--- Frontend/src/components/Header/Header.jsx
+++ Frontend/src/components/Header/Header.jsx
@@ -13,32 +13,34 @@ import { CartContext } from "../../contexts/CartProvider";
 
 
 export default function Header() {
-  const {cart} = useContext(CartContext)
+  const { cart } = useContext(CartContext)
   return (
     <VStack width={"100%"}>
-      <Flex  width={"100%"} justifyContent={"space-between"} px={" 60px"} py={"4px"} background={"#7c189f"} >
-        <Link color="white" _hover={{textDecoration:"underline"}}>SPRING CYBER WEEK | UP TO 80% OFF ENDS TOMORROW <FaArrowRight color="white" /></Link>
-        <Link color="white" _hover={{textDecoration:"underline"}}>Rewards | Financing | Professional | Everything Ships FREE*</Link>
+      <Flex width={"100%"} justifyContent={"space-between"} px={" 60px"} py={"4px"} background={"#7c189f"} >
+        <Link color="white" _hover={{ textDecoration: "underline" }}>SPRING CYBER WEEK | UP TO 80% OFF ENDS TOMORROW <FaArrowRight color="white" /></Link>
+        <Link color="white" _hover={{ textDecoration: "underline" }}>Rewards | Financing | Professional | Everything Ships FREE*</Link>
       </Flex>
       <Flex width={"100%"} justifyContent={"space-around"} px={" 60px"} py={"4px"}>
         <Flex width={"15%"}>
-        <Text fontWeight="bold" fontSize="28px" color="#7c189f">Wayfair</Text>
+          <Text fontWeight="bold" fontSize="28px" color="#7c189f">Wayfair</Text>
 
         </Flex>
         <Flex width={"70%"}>
           <SearchBar />
         </Flex>
         <Flex width={"30%"} gap={"20px"} justifyContent={"flex-end"}>
-          <Link href="/account" fontWeight={"500"}><MdOutlineAccountCircle  _hover={{color:"#7c189f"}} size={30} /></Link><AccountDropdown/>
-          <Link href="/cart" fontWeight={"500"} ><AiOutlineShoppingCart _hover={{color:"#7c189f" }} size={30}/>{cart.length}</Link>
+          <Link href="/account" fontWeight={"500"} >
+            <AccountDropdown />
+          </Link>
+          <Link href="/cart" fontWeight={"500"} ><AiOutlineShoppingCart _hover={{ color: "#7c189f" }} size={30} />
+          {cart.length===0 ? "": cart.length}</Link>
         </Flex>
       </Flex>
-      
+
     </VStack>
   );
 }
- 
 
 
 
-    
+
